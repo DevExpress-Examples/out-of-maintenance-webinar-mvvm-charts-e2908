1206261786407181ad73e444a670eca87850c619
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 20:35:39 +0400

    Change example status to out of maintenance

--- CS/MVVMCharts/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-namespace MVVMCharts {
-    public partial class MainPage : UserControl {
-        public MainPage() {
-            InitializeComponent();
-        }
-    }
-}
